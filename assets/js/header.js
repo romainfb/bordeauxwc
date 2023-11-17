8a84fb2c2e950045a29e4cbd2ff68f859bc20d6c
Saidi Yanis <yanis_contant@hotmail.fr>
2023-11-17 10:08:33 +0100

    This commit implements informational page

--- assets/js/header.js
+++ assets/js/header.js
@@ -5,10 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
           <nav>
             <ul class="flex space-x-6"> <!-- Agrandissement de l'espace entre les éléments -->
               <li>
-                <a href="#data" class="text-xl px-5 py-3 bg-mainColor text-white font-semibold rounded-lg shadow-md transition duration-300 hover:bg-white hover:text-black">Data</a> <!-- Texte plus grand et padding augmenté -->
+                <a href="C:/dev/Wcbordeaux/bordeauxwc/index.html" class="text-xl px-5 py-3 bg-mainColor text-white font-semibold rounded-lg shadow-md transition duration-300 hover:bg-white hover:text-black">Data</a> <!-- Texte plus grand et padding augmenté -->
               </li>
               <li>
-                <a href="#information" class="text-xl px-5 py-3 bg-mainColor text-white font-semibold rounded-lg shadow-md transition duration-300 hover:bg-white hover:text-black">Information</a>
+                <a href="C:/dev/Wcbordeaux/bordeauxwc/assets/html/informations.html" class="text-xl px-5 py-3 bg-mainColor text-white font-semibold rounded-lg shadow-md transition duration-300 hover:bg-white hover:text-black">Information</a>
               </li>
               <li>
                 <a href="#autre" class="text-xl px-5 py-3 bg-mainColor text-white font-semibold rounded-lg shadow-md transition duration-300 hover:bg-white hover:text-black">Autre</a>
